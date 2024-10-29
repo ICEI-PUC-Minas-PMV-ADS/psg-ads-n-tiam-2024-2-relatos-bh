e9e36af5f408a4add8d06da153a40e678a8f2c64
Gustavo Oliveira <gustavo.oliveira.1451719@sga.pucminas.br>
2024-10-29 11:54:20 -0300

    deleting old project

--- src/relata-bh/apiManager/Endpoints.ts
+++
@@ -1,6 +0,0 @@
-export const ENDPOINTS = {
-    LOGIN: () => BASE_URL + "auth/login",
-    REGISTER: () => BASE_URL + "auth/register"
-    //caminho para os outros endpoints ficarao aqui 
-}
-const BASE_URL = "https://relatabh.azurewebsites.net/"
