5c05b4dd3a890466fb5e922a95aa4c3eb2d6bb64
devleandrocosta <leandrohm.w@gmail.com>
2024-11-20 15:57:13 -0300

    End point busca por lat e log

--- src/RelataBH/RelataBH/Service/Relato/IRelatoService.cs
+++ src/RelataBH/RelataBH/Service/Relato/IRelatoService.cs
@@ -7,6 +7,8 @@ namespace RelataBH.Service.Relato
     public interface IRelatoService
     {
         public Task<IEnumerable<VW_RELATOS>> GetRelatos();
+        public Task<IEnumerable<VW_RELATOS>> GetRelatosPoint(string lat, string log);
+        public Task<VW_RELATOS> GetRelatoId(int Id);
         public Task<Model.Relato.Relato> SaveRelato(RelatoRequest relato);
         public Task<Model.Relato.Relato> UpdateRelato(RelatoRequest relato);
         public Task<bool> DeleteRelato(int id);
--- src/RelataBH/RelataBH/Service/Relato/IRelatoServiceImpl.cs
+++ src/RelataBH/RelataBH/Service/Relato/IRelatoServiceImpl.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using NetTopologySuite.Geometries;
 using RelataBH.Database;
 using RelataBH.Model.Relato;
 using RelataBH.Service.Auth.Domain.Relato;
+using System.Globalization;
+using System.Web.Http.Results;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RelataBH.Service.Relato
 {
@@ -10,6 +16,23 @@ namespace RelataBH.Service.Relato
         public async Task<IEnumerable<VW_RELATOS>> GetRelatos()
         {
             return await relatoContext.VW_RELATOS.ToListAsync();
+        }
+
+        public async Task<VW_RELATOS> GetRelatoId(int Id)
+        {
+            var relato =  await relatoContext.VW_RELATOS.FirstOrDefaultAsync(x => x.IdRelato == Id);
+            return relato;
+        }
+
+        public async Task<IEnumerable<VW_RELATOS>> GetRelatosPoint(string lat, string log)
+        {
+            var pointCenter = new Point(double.Parse(log, CultureInfo.InvariantCulture), double.Parse(lat, CultureInfo.InvariantCulture)){SRID = 4326};
+
+            var relatos = await relatoContext.VW_RELATOS
+                .Where(item => item.point.Distance(pointCenter) <= 0.025)
+                .ToListAsync();
+
+            return relatos;
         }
 
         public async Task<Model.Relato.Relato> SaveRelato(RelatoRequest relato)
