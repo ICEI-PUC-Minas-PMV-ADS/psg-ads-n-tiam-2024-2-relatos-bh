5fe8c60c6597056dac033835fffbd6f5b17eb3f3
armintas <armintasfilho@gmail.com>
2024-11-21 03:38:10 -0300

    Criation endpoint user relatos historic

---
+++ src/RelataBH/RelataBH/Service/Profile/RelatoHistoric/IRelatoHistoricService.cs
@@ -0,0 +1,7 @@
+﻿namespace RelataBH.Service.Profile.RelatoHistoric
+{
+    public interface IRelatoHistoricService
+    {
+        public Task<IEnumerable<Model.Relato.Relato>> GetRelatosByUser(int userId);
+    }
+}
