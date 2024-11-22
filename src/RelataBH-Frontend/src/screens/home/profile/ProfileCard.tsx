fbb8945425b6b65bee7c95acb91b5423cd49e300
armintas <armintasfilho@gmail.com>
2024-11-22 16:34:24 -0300

    ShimmerPlaceHolder

--- src/RelataBH-Frontend/src/screens/home/profile/ProfileCard.tsx
+++ src/RelataBH-Frontend/src/screens/home/profile/ProfileCard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { ActivityIndicator, Avatar, Button, Card, Divider, Text } from "react-native-paper";
 import { ProfileService } from "../../../services/profile/ProfileService";
+import ShimmerPlaceHolder from 'react-native-shimmer-placeholder'
 
 export const ProfileCard: React.FC = () => {
     const [profile, setProfile] = useState<Profile>();
@@ -22,31 +23,71 @@ export const ProfileCard: React.FC = () => {
     return (
         <View style={{ alignItems: 'center', width: '100%' }}>
 
-            {
-            loading ?
-                <View>
-                    <ActivityIndicator/>
-                </View>
-                :
-                <View style={{ alignItems: 'center' }}>
-                    <Avatar.Image
-                        size={150}
-                        source={require('../../../../assets/avatar-4x.jpg')}
-                        style={{ zIndex: 1, position: 'absolute', top: 0 }} />
 
-                    <Card style={{ marginTop: 75, paddingTop: 64, width: '100%', backgroundColor: '#FEF7FF' }}>
-                        <Card.Content>
-                            <Text variant="titleLarge" style={{ textAlign: 'center', color: '#65558F', fontWeight: 'bold' }}>{profile?.nome}</Text>
+            <ShimmerPlaceHolder
+                shimmerColors={["#E0E0E0", "#F5F5F5", "#E0E0E0"]}
+                visible={!loading}
+                duration={1500}
+                style={{
+                    width: 150,
+                    height: 150,
+                    borderRadius: 75,
+                    position: 'absolute',
+                    top: 0,
+                    zIndex: 1
+                }}
+            >
+                <Avatar.Image
+                    size={150}
+                    source={require('../../../../assets/avatar-4x.jpg')}
+                />
+            </ShimmerPlaceHolder>
+
+            <Card style={{ marginTop: 75, paddingTop: 64, width: '100%', backgroundColor: '#FEF7FF' }}>
+                <Card.Content>
+                    <View style={{ alignItems: 'center' }}>
+                        <ShimmerPlaceHolder
+                            shimmerColors={["#E0E0E0", "#F5F5F5", "#E0E0E0"]}
+                            visible={!loading}
+                            duration={1500}
+                            
+                        >
+                            < Text variant="titleLarge" style={{ textAlign: 'center', color: '#65558F', fontWeight: 'bold' }}>{profile?.nome}</Text>
+                        </ShimmerPlaceHolder>
+
+                        <ShimmerPlaceHolder
+                            shimmerColors={["#E0E0E0", "#F5F5F5", "#E0E0E0"]}
+                            visible={!loading}
+                            duration={1500}
+                            style={{ marginTop: 1 }}
+                        >
                             <Text variant="bodyMedium" style={{ textAlign: 'center', color: '#65558F' }}>{profile?.email}</Text>
-                            <Divider style={{ marginVertical: 12 }} />
-                            <Text variant="bodyMedium" style={{ textAlign: 'center', color: '#65558F' }}>Membro desde {new Intl.DateTimeFormat('pt-BR', { month: 'numeric', year: 'numeric' }).format(new Date(profile?.createdAt??0))}</Text>
+                        </ShimmerPlaceHolder>
+                    </View>
+
+                    <Divider style={{ marginVertical: 12 }} />
+
+                    <View style={{ alignItems: 'center' }}>
+                        <ShimmerPlaceHolder
+                            shimmerColors={["#E0E0E0", "#F5F5F5", "#E0E0E0"]}
+                            visible={!loading}
+                            duration={1500}
+                            style={{ marginTop: 1 }}
+                        >
+                            <Text variant="bodyMedium" style={{ textAlign: 'center', color: '#65558F' }}>Membro desde {new Intl.DateTimeFormat('pt-BR', { month: 'numeric', year: 'numeric' }).format(new Date(profile?.createdAt ?? 0))}</Text>
+                        </ShimmerPlaceHolder>
+
+                        <ShimmerPlaceHolder
+                            shimmerColors={["#E0E0E0", "#F5F5F5", "#E0E0E0"]}
+                            visible={!loading}
+                            duration={1500}
+                            style={{ marginTop: 1 }}
+                        >
                             <Text variant="bodyMedium" style={{ textAlign: 'center', color: '#65558F' }}>{profile?.qntRelatos} relatos feitos para a comunidade</Text>
-                        </Card.Content>
-                    </Card>
-                </View>
-            }
-
-
+                        </ShimmerPlaceHolder>
+                    </View>
+                </Card.Content>
+            </Card>
         </View>
     );
 }
